1ee274c4b418572dca83e17eaf8b45007959feb1
bamb <bamb.bakang@gmail.com>
2021-04-20 00:22:11 -0400

    Adds analog clock not working yet.

--- src/app/analog-clock/analog-clock.component.ts
+++ src/app/analog-clock/analog-clock.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, Input, OnDestroy, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-analog-clock',
@@ -9,32 +9,33 @@ export class AnalogClockComponent implements AfterViewInit, OnDestroy {
 
 
   @Input() currentTime: number;
-  @ViewChild('hourHand') hourHand: HTMLElement;
-  @ViewChild('minHand') minHand: HTMLElement;
-  @ViewChild('secondHand') secondHand: HTMLElement;
+  @ViewChild('hourHand') hourHand: ElementRef;
+  @ViewChild('minHand') minHand: ElementRef;
+  @ViewChild('secondHand') secondHand: ElementRef;
   private intervalId;
 
   constructor() {
-    this.currentTime = Date.now();
   }
 
   ngAfterViewInit() {
-    this.updateClockFace();
-    this.intervalId = setInterval(() => this.updateClockFace(), 100);
+    this.currentTime = Date.now();
+    this.intervalId = setInterval(() => this.refreshClock(), 1000);
   }
 
   ngOnDestroy() {
     clearInterval(this.intervalId);
   }
 
-  updateClockFace(): void {
+  private refreshClock(): void {
+    this.currentTime += 1000;
     const date = new Date(this.currentTime);
     const hDeg = date.getHours() * 30 + date.getMinutes() * (360 / 720);
     const mDeg = date.getMinutes() * 6 + date.getSeconds() * (360 / 3600);
     const sDeg = date.getSeconds() * 6;
-    this.hourHand.setAttribute('style', 'transform: rotate(' + hDeg + 'deg\')');
-    this.minHand.setAttribute('style', 'transform: rotate(' + mDeg + 'deg\')');
-    this.secondHand.setAttribute('style', 'transform: rotate(' + sDeg + 'deg\')');
+    console.log(sDeg);
+    this.hourHand.nativeElement.setAttribute('style', 'transform: rotate(' + hDeg + 'deg\')');
+    this.minHand.nativeElement.setAttribute('style', 'transform: rotate(' + mDeg + 'deg\')');
+    this.secondHand.nativeElement.setAttribute('style', 'transform: rotate(' + sDeg + 'deg\')');
   }
 
 }
